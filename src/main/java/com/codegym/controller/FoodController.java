5888a53bfb9dc306067fc8e10d158ab7473607e5
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-07-27 14:24:46 +0700

    show create form

--- src/main/java/com/codegym/controller/FoodController.java
+++ src/main/java/com/codegym/controller/FoodController.java
@@ -3,14 +3,28 @@ package com.codegym.controller;
 import com.codegym.service.food.IFoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/foods")
 public class FoodController {
     @Autowired
     IFoodService foodService;
+
+    @GetMapping("")
+    public ModelAndView showList(){
+        ModelAndView modelAndView=new ModelAndView("/food/list");
+        modelAndView.addObject("foods",foodService.findAll());
+        return modelAndView;
+    }
+
+    @RequestMapping("/create")
+    public ModelAndView showCreateForm(){
+
+    }
 
 
 }
