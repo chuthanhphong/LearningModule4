7701895d522b491a31b329991ae05ae5f12d2003
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-07-27 15:23:43 +0700

    .

--- src/main/java/com/codegym/controller/FoodController.java
+++ src/main/java/com/codegym/controller/FoodController.java
@@ -1,11 +1,14 @@
 package com.codegym.controller;
 
+import com.codegym.model.Food;
 import com.codegym.service.food.IFoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,6 +26,15 @@ public class FoodController {
 
     @RequestMapping("/create")
     public ModelAndView showCreateForm(){
+        ModelAndView modelAndView=new ModelAndView("/food/create");
+
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public String create(Food food, @RequestParam MultipartFile file){
+        foodService.save(food);
+        return "redirect:/foods";
 
     }
 
